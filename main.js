a9b11abc424bd95d5936ccb73cee2ec64ff40ab8
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-10-11 20:04:10 +0530

    commit 10 = 3 hours JS Project (Integrating REST API) - Restaurant booking App

--- main.js
+++ main.js
@@ -1,59 +1,74 @@
-//Saving the user Details on Crud Crud
-//Update the user Details = 
+//3 hours JS Project (Integrating REST API) = Stock Mangement Tool 
 
 const myForm = document.querySelector('#my-form');
-const nameInput = document.getElementById('name');
-const emailInput = document.querySelector('#email');
+const priceInput = document.getElementById('price');
+const chooseDishInput = document.getElementById('chooseDish');
+var option = document.querySelector('#tableOption');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-const itemList = document.getElementById('items');
-myForm.addEventListener('submit', onSubmit);
-var idOfUsersUpdatingDetails;
+const table1 = document.querySelector('#table1');
+const table2 = document.querySelector('#table2');
+const table3 = document.querySelector('#table3');
+
+myForm.addEventListener('submit', addItemsToServer);
 
 function showNewUserOnScreen(res) {
-    console.log('res.name = ' + res.name);
-    console.log('res.email = ' + res.email);
     console.log('id = ' + res._id);
+    console.log('res.price = ' + res.price);
+    console.log('res.dish = ' + res.dish);
+    console.log('res.table = ' + res.table);
 
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${res.name}: ${res.email}`));
-    userList.appendChild(li);
-
-    //for adding delete button functionality 
-    const delBtn = document.createElement('input');
-    delBtn.setAttribute('type', 'button');
-    delBtn.setAttribute('value', 'Delete');
-    //setting id as an email of user so we can pass the value
-    delBtn.id = res._id;
-    delBtn.setAttribute('onclick', 'deleteUser(this)');
-    li.appendChild(delBtn);
-    delBtn.style.margin = '10px';
-    delBtn.style.marginLeft = '10px';
-    delBtn.style.padding = '5px';
-    delBtn.style.fontSize = '15px';
-    delBtn.style.backgroundColor = '#333';
-    delBtn.style.color = 'white';
-
-    //for adding edit button functionality 
-    const editBtn = document.createElement('input');
-    editBtn.setAttribute('type', 'button');
-    editBtn.setAttribute('value', 'Edit');
-    editBtn.id = res._id;
-    editBtn.setAttribute('onclick', 'editUser(this)');
-    li.appendChild(editBtn);
-    editBtn.style.margin = '10px';
-    editBtn.style.marginLeft = '10px';
-    editBtn.style.padding = '5px';
-    editBtn.style.fontSize = '15px';
-    editBtn.style.backgroundColor = '#333';
-    editBtn.style.color = 'white';
+    if (res.table === 'Table 1') {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(` ${res.price}: ${res.table} : ${res.dish}`));
+        table1.appendChild(li);
+        //delete button
+        const delBtn = document.createElement('input');
+        delBtn.setAttribute('type', 'button');
+        delBtn.setAttribute('value', 'Delete Order 1');
+        delBtn.id = res._id;
+        delBtn.setAttribute('onclick', 'deleteOrder(this)');
+        li.appendChild(delBtn);
+        delBtn.style.padding = '8px';
+        delBtn.style.backgroundColor = 'floralwhite';
+        delBtn.style.marginLeft = '40px';
+    }
+    else if (res.table === 'Table 2') {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(` ${res.price}: ${res.table} : ${res.dish}`));
+        table2.appendChild(li);
+        //delete button
+        const delBtn = document.createElement('input');
+        delBtn.setAttribute('type', 'button');
+        delBtn.setAttribute('value', 'Delete Order 2');
+        delBtn.id = res._id;
+        delBtn.setAttribute('onclick', 'deleteOrder(this)');
+        li.appendChild(delBtn);
+        delBtn.style.padding = '8px';
+        delBtn.style.backgroundColor = 'floralwhite';
+        delBtn.style.marginLeft = '40px';
+    }
+    else if (res.table === 'Table 3') {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(` ${res.price}: ${res.table} : ${res.dish}`));
+        table3.appendChild(li);
+        //delete button
+        const delBtn = document.createElement('input');
+        delBtn.setAttribute('type', 'button');
+        delBtn.setAttribute('value', 'Delete Order 3');
+        delBtn.id = res._id;
+        delBtn.setAttribute('onclick', 'deleteOrder(this)');
+        li.appendChild(delBtn);
+        delBtn.style.padding = '8px';
+        delBtn.style.backgroundColor = 'floralwhite';
+        delBtn.style.marginLeft = '40px';
+    }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    axios.get('https://crudcrud.com/api/b077c957e09b462fae3d62ada9d66da5/appointmentData')
+    axios.get('https://crudcrud.com/api/b7b977f588c449fcafe596747a25efff/BookTable')
         .then(res => {
             //this will print array collection with data output of get request
-            // console.log(res);
+            console.log(res);
 
             //res is an object of array 
             for (let i = 0; i < res.data.length; i++) {
@@ -65,134 +80,101 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 })
 
-function onSubmit(e) {
+function addItemsToServer(e) {
     e.preventDefault();
+    console.log('option = ' + option.value);
+    let obj = {
+        price: priceInput.value,
+        dish: chooseDishInput.value,
+        table: option.value,
+    };
 
-    if (nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
-        setTimeout(() => msg.remove(), 3000);
-    } else {
+    axios.post('https://crudcrud.com/api/b7b977f588c449fcafe596747a25efff/BookTable', obj)
+        .then(res => {
+            console.log('after post = ');
+            console.log(res);
 
-        //if valu of this idOfUsersUpdatingDetails variable is set then update otherwise submit new details
-        if (idOfUsersUpdatingDetails != undefined) {
-            console.log('idOfUsersUpdatingDetails = ' + idOfUsersUpdatingDetails);
-            let nameInputUpdate = document.getElementById('name').value;
-            let emailInputUpdate = document.querySelector('#email').value;
+            if (res.data.table === 'Table 1') {
+                console.log('Table 1');
+                console.log('id = ' + res.data._id);
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(` ${res.data.price}: ${res.data.table} : ${res.data.dish}`));
+                table1.appendChild(li);
+                //button
+                const delBtn = document.createElement('input');
+                delBtn.setAttribute('type', 'button');
+                delBtn.setAttribute('value', 'Delete Order 1');
+                delBtn.id = res.data._id;
+                delBtn.setAttribute('onclick', 'deleteOrder(this)');
+                li.appendChild(delBtn);
+                delBtn.style.padding = '8px';
+                delBtn.style.backgroundColor = 'floralwhite';
+                delBtn.style.marginLeft = '40px';
 
-            //update values on BE 
-            axios.put('https://crudcrud.com/api/b077c957e09b462fae3d62ada9d66da5/appointmentData/' + idOfUsersUpdatingDetails, {
-                name: nameInputUpdate,
-                email: emailInputUpdate,
-            })
-                .then(res => {
-                    console.log(res);
-                    //keeping this undefined will help you submit the data instead of updating
-                    idOfUsersUpdatingDetails = undefined;
-                    //after submiting will remove the details inside the input bosex
-                    nameInput.value = '';
-                    emailInput.value = '';
-                    //old name and email values in the li tags will be removed
-                    location.reload();
-                    // location.replace('https://www.w3schools.com/howto/howto_js_redirect_webpage.asp');
-                }).catch(err => console.log(err + ' error'));
+            }
+            else if (res.data.table === 'Table 2') {
+                console.log('Table 2');
+                console.log('id = ' + res.data._id);
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(` ${res.data.price}: ${res.data.table} : ${res.data.dish}`));
+                table2.appendChild(li);
+                // button
+                const delBtn = document.createElement('input');
+                delBtn.setAttribute('type', 'button');
+                delBtn.setAttribute('value', 'Delete Order 2');
+                delBtn.id = res.data._id;
+                delBtn.setAttribute('onclick', 'deleteOrder(this)');
+                li.appendChild(delBtn);
+                delBtn.style.padding = '8px';
+                delBtn.style.backgroundColor = 'floralwhite';
+                delBtn.style.marginLeft = '40px';
 
-        }
-        //add the new user details to the BE server
-        else {
-            msg.classList.add('success');
-            msg.innerHTML = 'Successfully loged in';
-            setTimeout(() => msg.remove(), 2000);
+            } else if (res.data.table === 'Table 3') {
+                console.log('Table 3');
+                console.log('id = ' + res.data._id);
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(` ${res.data.price}: ${res.data.table} : ${res.data.dish}`));
+                table3.appendChild(li);
+                //button
+                const delBtn = document.createElement('input');
+                delBtn.setAttribute('type', 'button');
+                delBtn.setAttribute('value', 'Delete Order 3');
+                delBtn.id = res.data._id;
+                delBtn.setAttribute('onclick', 'deleteOrder(this)');
+                li.appendChild(delBtn);
+                delBtn.style.padding = '8px';
+                delBtn.style.backgroundColor = 'floralwhite';
+                delBtn.style.marginLeft = '40px';
+            }
 
-            let obj = {
-                name: nameInput.value,
-                email: emailInput.value
-            };
-
-            //store the data on the server
-            axios.post('https://crudcrud.com/api/b077c957e09b462fae3d62ada9d66da5/appointmentData', obj)
-                .then(res => {
-                    console.log(res);
-                    //to set the data of list with required details
-                    const li = document.createElement('li');
-                    li.appendChild(document.createTextNode(`${res.data.name}: ${res.data.email}`));
-                    userList.appendChild(li);
-
-                    //for adding delete button functionality 
-                    const delBtn = document.createElement('input');
-                    delBtn.setAttribute('type', 'button');
-                    delBtn.setAttribute('value', 'Delete');
-                    //setting id as an email of user so we can pass the value
-                    delBtn.id = res.data._id;
-                    delBtn.setAttribute('onclick', 'deleteUser(this)');
-                    li.appendChild(delBtn);
-                    delBtn.style.margin = '10px';
-                    delBtn.style.marginLeft = '10px';
-                    delBtn.style.padding = '5px';
-                    delBtn.style.fontSize = '15px';
-                    delBtn.style.backgroundColor = '#333';
-                    delBtn.style.color = 'white';
-
-                    //for adding edit button functionality 
-                    const editBtn = document.createElement('input');
-                    editBtn.setAttribute('type', 'button');
-                    editBtn.setAttribute('value', 'Edit');
-                    editBtn.id = res.data._id;
-                    editBtn.setAttribute('onclick', 'editUser(this)');
-                    li.appendChild(editBtn);
-                    editBtn.style.margin = '10px';
-                    editBtn.style.marginLeft = '10px';
-                    editBtn.style.padding = '5px';
-                    editBtn.style.fontSize = '15px';
-                    editBtn.style.backgroundColor = '#333';
-                    editBtn.style.color = 'white';
-
-                }).catch(err => console.log(err + ' error'));
-
-            nameInput.value = '';
-            emailInput.value = '';
-        }//submit
-    }//else
+            priceInput.value = '';
+            chooseDishInput.value = '';
+            option.value = '';
+        }).catch(err => console.log(err));
 
 }//onSubmit
 
-//remove user details and to fetch name and email from BE server
-function editUser(val) {
-    console.log('val = ' + val.id);
-
-    //to get all the details neccessary for editing
-    axios.get('https://crudcrud.com/api/b077c957e09b462fae3d62ada9d66da5/appointmentData/' + val.id)
+function deleteOrder(val) {
+    // console.log('val = ' + val.id);
+    axios.delete('https://crudcrud.com/api/b7b977f588c449fcafe596747a25efff/BookTable/' + val.id)
         .then(res => {
             console.log(res);
-            //to set the user details in the input boxes
-            nameInput.value = res.data.name;
-            emailInput.value = res.data.email;
-
-
-            //global value of this var will help for updating
-            idOfUsersUpdatingDetails = val.id;
-
         }).catch(err => console.log(err + ' error'));
 
-}
-
-function deleteUser(val) {
-    if (confirm('Are You Sure?')) {
-        //this will delete the record from BE server    
-        // console.log('val = ' + val.id);
-        axios.delete('https://crudcrud.com/api/b077c957e09b462fae3d62ada9d66da5/appointmentData/' + val.id)
-            .then(res => {
-                console.log(res);
-            }).catch(err => console.log(err + ' error'));
-
-        //get a parent node element and delete the user details from the list
-        let parentEle = document.getElementById(val.id).parentElement;
-        userList.removeChild(parentEle);
+    //get a parent node element and delete the user details from the list
+    let parentEle = document.getElementById(val.id).parentElement;
+    if (val.value === 'Delete Order 1') {
+        console.log('delete from Table 1');
+        table1.removeChild(parentEle);
     }
-}
-
-
-
+    else if (val.value === 'Delete Order 2') {
+        console.log('delete from Table 2');
+        table2.removeChild(parentEle);
+    } else if (val.value === 'Delete Order 3') {
+        console.log('delete from Table 3');
+        table3.removeChild(parentEle);
+    }
+}//delete
 
 
 
